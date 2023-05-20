50e5e4b9366f3ceefb98778d25a35c8efa927d0c
GGbrie <106721167+GGbrie@users.noreply.github.com>
2023-05-20 00:50:08 -0600

    Add files via upload

---
+++ Desafio/Base-de-datos-desafio/desafio base de dato/Alumno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base_de_Datos_clase
+{
+    class Alumno
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String Telefono { get; set; }
+        public String Direccion { get; set; }
+        public String Carnet { get; set; }
+        public int Edad { get; set; }
+    }
+}
